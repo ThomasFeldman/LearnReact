adabaf656149ae76e1782507db3d935e6e6648f0
ThomasFeldman <tfeldman1.31@gmail.com>
2020-10-26 18:29:01 -0500

    faker and semantic ui practice

--- test_app/src/index.js
+++ test_app/src/index.js
@@ -1,24 +1,65 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 // Create a react component
 const App = () => {
-    const buttonText ={text : 'Click Me!'};
-
     return (
-        <div>
-            <label className="label" htmlFor="name">Enter name:</label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>
-                {buttonText.text}
-            </button>
+        // Comment #1
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+
+
+        {/*Comment #2 */}
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.avatar()} />
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00PM</span>
+                </div>
+                <div className="text">Nice blog post!</div>
+            </div>
         </div>
+
+
+
+        {/* //Comment #3 */}
+        <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.avatar()} />
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    Sam
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00PM</span>
+                </div>
+                <div className="text">Nice blog post!</div>
+            </div>
+        </div>
+        </div>
+
     );
 };
 
 // Take a react component and show it on the screen
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'))
