a0a01613922b183ed7700ddc26c2eeb224263a0f
ThomasFeldman <tfeldman1.31@gmail.com>
2020-11-01 23:20:07 -0600

    adding more class based functionality

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,16 +5,20 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
+        //This is the only time you would ever do direct assignment or "state = something",
+        //when you initialize it. To change values later on, always use this.setState({new value})
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err) => console.log(err)
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-    
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
