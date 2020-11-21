68c94567d5f94b8cfd94e2697bdac2c6cd8def71
ThomasFeldman <tfeldman1.31@gmail.com>
2020-11-20 22:02:18 -0600

    messing around with React Components

---
+++ myApp/src/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+    class Button extends React.Component {
+        render() {
+            return(
+            <div>
+                <button id="button">This is a class button</button>
+            </div>
+            ); 
+        };
+    }   
+    
+
+
+// const Button = (props) => {
+//     return (
+//         <div>
+//             <button>This is a button</button>
+//         </div>
+//     );    
+// };
+
+export default Button;
