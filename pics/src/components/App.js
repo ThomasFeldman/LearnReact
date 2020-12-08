9a2a536c15bc5244b6426fd3130ac08a24b58976
ThomasFeldman <tfeldman1.31@gmail.com>
2020-12-07 19:39:57 -0600

    Binding Callbacks

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,22 +3,23 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    
-    async onSearchSubmit(term){
+    state = { images: [] };
+    onSearchSubmit = async (term) =>{
         const response = await axios.get('https://api.unsplash.com/search/photos', {
           params: { query: term },
           headers: {
             Authorization: 'Client-ID CrM2fFC3OcHfuGTJnXRPfeQhgmDUIV9qjNb9AGu6YeE'
           }
         });
-
-        console.log(response.data.results);
+        
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: { this.state.images.length } images
             </div>
         );
     }
