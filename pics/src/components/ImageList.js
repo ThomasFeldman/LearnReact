ed67809b09f87c43f5ee0cc03e04d7f9c5f9ff34
ThomasFeldman <tfeldman1.31@gmail.com>
2020-12-19 21:31:12 -0600

    Fixed image width issue

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './ImageList.css';
 
+
 const ImageList = (props) => {
     const images = props.images.map((({description, id, urls}) => {
-        return <img alt={description} key={id} src={urls.regular} />
+        return <img className="image-list" alt={description} key={id} src={urls.regular} />
     }));
     return <div className="image-list">{images}</div>;
 };
