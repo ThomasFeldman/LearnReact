89d552728626ede89cf5cdc74054b857866298c7
ThomasFeldman <tfeldman1.31@gmail.com>
2020-12-23 21:51:11 -0600

    Creating Image Card Component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
+
 class App extends React.Component {
     state = { images: [] };
+
     onSearchSubmit = async (term) =>{
         const response = await unsplash.get('/search/photos', {
           params: { query: term },
@@ -12,6 +14,7 @@ class App extends React.Component {
         this.setState({ images: response.data.results });
     }
 
+    
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
