9b769035608d0ba8c3e5d694a00d3bb564609bf4
ThomasFeldman <tfeldman1.31@gmail.com>
2020-12-31 23:22:19 -0600

    Searching for Videos

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAEc76JT7HEsgdmTF47RRhQAzf0UP45vEE';
+
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
