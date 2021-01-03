9201e22815d055983a84db2af6273f2998c21609
ThomasFeldman <tfeldman1.31@gmail.com>
2021-01-02 23:29:55 -0600

    Something wrong with axios module, fixing this tomorrow

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAEc76JT7HEsgdmTF47RRhQAzf0UP45vEE';
 
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        type: 'video',
-        maxResults: 5,
-        key: KEY
-    }
 });
